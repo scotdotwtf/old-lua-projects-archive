cca6f1c0080859f1f10ad459903b3c68b9168446
spec <77427029+specowos@users.noreply.github.com>
2021-10-03 11:18:16 -0500

    Delete goodrickroll.lua

--- PROJECTS/adminware v1.2/goodrickroll.lua
+++
@@ -1,3 +0,0 @@
-video = loadstring(game:HttpGet("https://builderman.pro/pub/videoLib.lua?raw=true"))()
---// MUST BE WEBM LOL - SAVES IN WORKSPACE AS: VIDEO.WEBM
-video.play("https://builderman.pro/pub/qsil9.webm")
