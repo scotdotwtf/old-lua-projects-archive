8250ef1734ec307fc65750afbb08f8b846e97487
spec <77427029+specowos@users.noreply.github.com>
2021-10-03 11:40:44 -0500

    Update owox load.lua

--- PROJECTS/owox [custom shiba]/owox load.lua
+++ PROJECTS/owox [custom shiba]/owox load.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/owox-aka-custom-shiba/main/owox%20src.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/old-lua-projects-archive/main/PROJECTS/owox%20%5Bcustom%20shiba%5D/owox%20src.lua'))()
