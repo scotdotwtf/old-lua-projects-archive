fab31124ce0b5a350967d7d83060622c9ff9f550
spec <77427029+specowos@users.noreply.github.com>
2021-10-03 11:15:14 -0500

    Update loadstring.lua

--- PROJECTS/adminware v1.2/loadstring.lua
+++ PROJECTS/adminware v1.2/loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/specowos/adminware-v1.2/main/source", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/specowos/old-lua-projects-archive/main/PROJECTS/adminware%20v1.2/source.lua", true))()
