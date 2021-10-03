7da0bbc51cc8aea0e4836f8d2fd211e37a424512
spec <77427029+specowos@users.noreply.github.com>
2021-10-03 11:38:31 -0500

    Update loadstring.lua

--- PROJECTS/project admin [better cmd list]/loadstring.lua
+++ PROJECTS/project admin [better cmd list]/loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/specowos/projectadmin/main/bettercmdlol", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/specowos/old-lua-projects-archive/main/PROJECTS/project%20admin%20%5Bbetter%20cmd%20list%5D/bettercmdlol.lua", true))()
