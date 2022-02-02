71517fd6896de2ccf8a828581233d59ba5127c1c
spec <77427029+specowos@users.noreply.github.com>
2022-02-01 23:14:46 -0600

    Create Load.lua

---
+++ PROJECTS/FE Skateboard (NOT MADE BY ME)/Load.lua
@@ -0,0 +1,24 @@
+--[[
+________  ________  ___  ___  ________  _______   ________  ___  __    ________  _________  _______      
+|\   ____\|\   __  \|\  \|\  \|\   ____\|\  ___ \ |\   ____\|\  \|\  \ |\   __  \|\___   ___\\  ___ \    
+\ \  \___|\ \  \|\  \ \  \\\  \ \  \___|\ \   __/|\ \  \___|\ \  \/  /|\ \  \|\  \|___ \  \_\ \   __/|    
+\ \_____  \ \   __  \ \  \\\  \ \  \    \ \  \_|/_\ \_____  \ \   ___  \ \   __  \   \ \  \ \ \  \_|/__  
+ \|____|\  \ \  \ \  \ \  \\\  \ \  \____\ \  \_|\ \|____|\  \ \  \\ \  \ \  \ \  \   \ \  \ \ \  \_|\ \
+   ____\_\  \ \__\ \__\ \_______\ \_______\ \_______\____\_\  \ \__\\ \__\ \__\ \__\   \ \__\ \ \_______\
+  |\_________\|__|\|__|\|_______|\|_______|\|_______|\_________\|__| \|__|\|__|\|__|    \|__|  \|_______|
+  \|_________|                                      \|_________|                                                                            
+]]--
+
+SKATEBOARD_HAT  = "COLDSTARESKATEBOARD"  -- if you want to change this, look in dex and get the name of the hat
+AUTOCHOOSE = false  -- auto chooses hat
+
+SPEED            = 32
+JUMP            = 41
+ACCELERATION    = 0.4
+STEERABILITY    = 0.7
+GRIP            = 0
+
+OFFSET = CFrame.Angles(math.rad(-90),math.rad(0),math.rad(45))   -- Change the last number to 45 if it is weird or 135 in some cases
+HOLD_OFFSET = CFrame.Angles(math.rad(0),math.rad(80),math.rad(45))
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/saucekid/scripts/main/sauceskate.lua"))()
