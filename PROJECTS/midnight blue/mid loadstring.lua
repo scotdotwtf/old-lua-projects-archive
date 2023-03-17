65af3b9dd8f2b7d3b92c9e5b3c1b965dbf802491
scotdotwtf <77427029+scotdotwtf@users.noreply.github.com>
2023-03-16 19:26:19 -0500

    Update mid loadstring.lua

--- PROJECTS/midnight blue/mid loadstring.lua
+++ PROJECTS/midnight blue/mid loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/specowos/old-lua-projects-archive/main/PROJECTS/midnight%20blue/mid%20config.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/scotdotwtf/old-lua-projects-archive/main/PROJECTS/midnight%20blue/mid%20config.lua"))()
