e200f99b97f0c584fcc6b3c93959035e634a8742
scotdotwtf <77427029+scotdotwtf@users.noreply.github.com>
2023-03-16 19:25:51 -0500

    Update loadstring for custom ui linorix.lua

--- PROJECTS/linorix [custom ui]/loadstring for custom ui linorix.lua
+++ PROJECTS/linorix [custom ui]/loadstring for custom ui linorix.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/specowos/old-lua-projects-archive/main/PROJECTS/linorix%20%5Bcustom%20ui%5D/linorix%20red%20ui%20source.lua'),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/specscotdotwtfowos/old-lua-projects-archive/main/PROJECTS/linorix%20%5Bcustom%20ui%5D/linorix%20red%20ui%20source.lua'),true))()
 				game.StarterGui:SetCore("SendNotification", {
 				Title = "Notification";
 				Text = "Hey! Spec here, just telling you that this script is broken due to pastebin deleting most of the scripts inside and also roblox updates";
