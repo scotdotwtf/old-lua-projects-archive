809ae78f8bdccc43a689bcecefb2682d6923915c
scotdotwtf <77427029+scotdotwtf@users.noreply.github.com>
2023-03-16 19:24:58 -0500

    Update loadstring.lua

--- PROJECTS/adminware v1.2/loadstring.lua
+++ PROJECTS/adminware v1.2/loadstring.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/specowos/old-lua-projects-archive/main/PROJECTS/adminware%20v1.2/source.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/scotdotwtf/old-lua-projects-archive/main/PROJECTS/adminware%20v1.2/source.lua", true))()
