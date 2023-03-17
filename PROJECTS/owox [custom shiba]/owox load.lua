2cfea6c0c4b9c05ebe1f3af928df4867b1eb67a7
scotdotwtf <77427029+scotdotwtf@users.noreply.github.com>
2023-03-16 19:27:12 -0500

    Update owox load.lua

--- PROJECTS/owox [custom shiba]/owox load.lua
+++ PROJECTS/owox [custom shiba]/owox load.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/specowos/old-lua-projects-archive/main/PROJECTS/owox%20%5Bcustom%20shiba%5D/owox%20src.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/scotdotwtf/old-lua-projects-archive/main/PROJECTS/owox%20%5Bcustom%20shiba%5D/owox%20src.lua'))()
